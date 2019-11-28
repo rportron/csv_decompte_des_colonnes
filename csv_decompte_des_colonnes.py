c2a2aaf9b2849c718925c8308a8c99e3389da7bd
rportron <github@portron.org>
2019-11-28 15:06:32 +0100

    Ajout de la gestion des arguments
    Ajout de la gestion des arguments : possibilité d'avoir le séparateur csv de son choix

--- csv_decompte_des_colonnes.py
+++ csv_decompte_des_colonnes.py
@@ -3,21 +3,30 @@
 Ce programme compte le nombre de colonnes de tout un fichier csv
     * donne le nombre de colonne
     * avertit si une ligne a un nmobre différent de colonnes
-Création du fichier le 5 novembre 2019
+Création du programme le 5 novembre 2019
+Dernière modification le 28 novembre 2019
 """
 import csv
-from sys import argv
-print("Ce programme compte le nombre de colonnes d'un fichier csv, il affiche le nombre de colonnes et avertit si certaines lignes n'ont pas le même nombre de colonnes")
-try:
-    print(" Décompte du nombre de colonne du fichier ", argv[1])
-except IndexError:
-    print("\nERREUR : il manque le fichier csv à traiter.\nLe programme va s'arrêter.")
-    exit()
+import argparse #gestion des arguments
+
+parser = argparse.ArgumentParser(description="Compte le nombre de colonnes d'un fichier csv")
+parser.add_argument('fichier', help='le fichier dont on doit dénombrer les colonnes')
+parser.add_argument('-sep', nargs='?', help="le séparateur du fichier csv, par défaut il s'agit du ;")
+args = parser.parse_args()
+
+fichier = args.fichier
+if args.sep:
+    delimiter = args.sep
+else:
+    delimiter = ';'
+
+print("\nCe programme compte le nombre de colonnes d'un fichier csv, il affiche le nombre de colonnes et avertit si certaines lignes n'ont pas le même nombre de colonnes\n")
+
 premiere_ligne = True
 probleme_de_nombre_de_colonnes = False
 ligne_en_cours = 1
-with open(argv[1], newline='') as f:
-        reader = csv.reader(f, delimiter=';')
+with open(fichier, newline='') as f:
+        reader = csv.reader(f, delimiter=delimiter)
         for row in reader:
             if premiere_ligne:
                 nbre_colonne = len(row)
@@ -29,6 +38,6 @@ with open(argv[1], newline='') as f:
                 print(" -- un nombre de colonne différent ({}) a été détecté à la ligne {}".format(nbre_colonne, ligne_en_cours))
             ligne_en_cours += 1
 if probleme_de_nombre_de_colonnes:
-    print("\nLe fichier {} n'a pas un nombre constant de colonnes :(".format(argv[1]))
+    print("\nLe fichier {} n'a pas un nombre constant de colonnes :(".format(fichier))
 else:
-    print("\nLe fichier {} a un nombre constant de colonnes :)".format(argv[1]))
+    print("\nLe fichier {} a un nombre constant de colonnes :)".format(fichier))
